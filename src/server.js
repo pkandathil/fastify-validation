3b3571f0e413361912cf05a97641a864e749c097
Prashant Kandathil <prashantgeorge36@gmail.com>
2023-03-28 20:56:12 -0700

    Modified start command to listen on any ip

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const server = require('./app')({
   }
 })
 
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ port: 3000, host: "::" }, (err, address) => {
   if (err) {
     server.log.error(err)
     process.exit(1)
