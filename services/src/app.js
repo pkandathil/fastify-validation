ce88d5cc1eb4d71930ee22e8906848d7b86fc20a
Prashant Kandathil <prashantgeorge36@gmail.com>
2023-03-28 20:07:43 -0700

    added route for health check

--- services/src/app.js
+++ services/src/app.js
@@ -38,7 +38,7 @@ function build(opts = {}) {
     res.status(200).send('Up and running')
   })
 
-  app.get('/test', (req, res) => {
+  app.get('/', (req, res) => {
     res.status(200).send('Up and running')
   })
 
